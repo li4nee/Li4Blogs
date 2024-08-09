5ea8e53c695da1c6ba0f3a60c68288b4bd7b86f7
Li4nee <nishantpokharel9@gmail.com>
2024-08-09 21:50:39 +0545

    Added cloudinary + FIxed a lot of shit.

--- controllers/blog.controller.js
+++ controllers/blog.controller.js
@@ -1,89 +1,156 @@
 import Blog from "../models/blog.models.js";
 import Comment from "../models/comment.models.js";
+import cloudinary from "../utils/cloudinary.js";
 
 const addBlog = async (req, res) => {
   const { title, body } = req.body;
-  const blog = await Blog.create({
-    title,
-    body,
-    coverImageUrl: `/uploads/${req.file.filename}`,
-    creator: req.user._id,
-  });
-  return res.redirect(`/blog/${blog._id}`);
+
+  try {
+    let coverImageUrl = "/images/default.jpg";
+    if (req.file) {
+      try {
+        const result = await cloudinary.uploader.upload(req.file.path, {
+          folder: "blogs",
+        });
+        coverImageUrl = result.secure_url;
+      } catch (cloudinaryError) {
+        console.error("Error uploading image to Cloudinary:", cloudinaryError);
+        return res.status(500).send("Error uploading cover image");
+      }
+    }
+
+    const blog = await Blog.create({
+      title,
+      body,
+      coverImageUrl,
+      creator: req.user._id,
+    });
+
+    return res.redirect(`/blog/${blog._id}`);
+  } catch (error) {
+    console.error("Error creating blog:", error);
+    return res.status(500).send("Error creating blog");
+  }
 };
 
 const handleBlogs = async (req, res) => {
-  const blog = await Blog.findById(req.params.id).populate("creator");
-  const comment = await Comment.find({ commentedOn: req.params.id }).populate(
-    "commentedBy"
-  );
-  const total = comment.length;
-  return res.render("blog", {
-    user: req.user,
-    blog: blog,
-    comment: comment,
-    total: total,
-  });
+  try {
+    const blog = await Blog.findById(req.params.id).populate("creator");
+    if (!blog) {
+      return res.status(404).send("Blog not found");
+    }
+
+    const comments = await Comment.find({
+      commentedOn: req.params.id,
+    }).populate("commentedBy");
+    const total = comments.length;
+
+    return res.render("blog", {
+      user: req.user,
+      blog: blog,
+      comments: comments,
+      total: total,
+    });
+  } catch (error) {
+    console.error("Error handling blog:", error);
+    return res.status(500).send("Internal Server Error");
+  }
 };
 
 const handleComment = async (req, res) => {
-  const comment = await Comment.create({
-    content: req.body.content,
-    commentedOn: req.params.blogID,
-    commentedBy: req.user._id,
-  });
-  return res.redirect(`/blog/${req.params.blogID}`);
+  try {
+    await Comment.create({
+      content: req.body.content,
+      commentedOn: req.params.blogID,
+      commentedBy: req.user._id,
+    });
+
+    return res.redirect(`/blog/${req.params.blogID}`);
+  } catch (error) {
+    console.error("Error creating comment:", error);
+    return res.status(500).send("Internal Server Error");
+  }
 };
 
 const deleteBlog = async (req, res) => {
-  console.log("hey");
   try {
     const blogId = req.params.blogID;
 
     const deleted = await Blog.findByIdAndDelete(blogId);
-    if (deleted) {
-      const blogs = await Blog.find({ creator: req.params.id })
-        .sort({ createdAt: -1 })
-        .populate("creator");
-
-      return res.render("userprofile", { user: req.user, blogs });
+    if (!deleted) {
+      return res.status(404).send("Blog not found");
     }
 
-    return res.status(404).send("error!!");
-  } catch (err) {
-    return res.status(500).send("error!!");
+    const blogs = await Blog.find({ creator: req.params.id })
+      .sort({ createdAt: -1 })
+      .populate("creator");
+
+    return res.render("userprofile", { user: req.user, blogs });
+  } catch (error) {
+    console.error("Error deleting blog:", error);
+    return res.status(500).send("Internal Server Error");
   }
 };
 
 const handleEditBlog = async (req, res) => {
   const { title, body } = req.body;
-  const newdata = {
-    title,
-    body
-  };
+  const newdata = { title, body };
+
   try {
-    const updatedBlog = await Blog.findOneAndUpdate({_id:req.params.blogId}, newdata, { new: true });
+    const updatedBlog = await Blog.findOneAndUpdate(
+      { _id: req.params.blogId },
+      newdata,
+      { new: true }
+    );
 
     if (updatedBlog) {
       return res.redirect(`/user/${req.user._id}`);
-    } 
+    } else {
+      return res.status(404).send("Blog not found");
+    }
   } catch (error) {
-    console.error('Error updating blog:', error);
+    console.error("Error updating blog:", error);
     return res.status(500).send("Internal Server Error");
   }
 };
 
-const changeCoverImage= async(req,res)=>{
+const changeCoverImage = async (req, res) => {
   try {
-    const updatedBlog = await Blog.findOneAndUpdate({_id:req.params.blogId},{coverImageUrl: `/uploads/${req.file.filename}`}, { new: true });
+    let coverImageUrl = "/images/default.jpg";
+    if (req.file) {
+      try {
+        const result = await cloudinary.uploader.upload(req.file.path, {
+          folder: "blogs",
+        });
+        coverImageUrl = result.secure_url;
+      } catch (cloudinaryError) {
+        console.error("Error uploading image to Cloudinary:", cloudinaryError);
+        return res.status(500).send("Error uploading cover image");
+      }
+    }
+
+    const updatedBlog = await Blog.findOneAndUpdate(
+      { _id: req.params.blogId },
+      { coverImageUrl },
+      { new: true }
+    );
 
     if (updatedBlog) {
       return res.redirect(`/blog/edit/${req.params.blogId}`);
-    } 
+    } else {
+      return res.status(404).send("Blog not found");
+    }
   } catch (error) {
-    console.error('Error updating blog:', error);
+    console.error("Error updating cover image:", error);
     return res.status(500).send("Internal Server Error");
   }
 };
 
-export { addBlog, handleComment, handleBlogs, deleteBlog ,handleEditBlog,changeCoverImage};
+export {
+  addBlog,
+  handleComment,
+  handleBlogs,
+  deleteBlog,
+  handleEditBlog,
+  changeCoverImage,
+};
--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -2,20 +2,36 @@ import { User } from "../models/user.models.js";
 import Blog from "../models/blog.models.js";
 import { generateToken, validateToken } from "../utils/auth.utils.js";
 import bcrypt from "bcryptjs";
+import cloudinary from "../utils/cloudinary.js";
+
 const signupUser = async (req, res) => {
   try {
     const { email, password, username } = req.body;
+    let profileImageUrl = "/images/default.jpg";
+
+    if (req.file) {
+      try {
+        const result = await cloudinary.uploader.upload(req.file.path, {
+          folder: "profiles",
+        });
+        profileImageUrl = result.secure_url;
+      } catch (cloudinaryError) {
+        console.error("Error uploading profile image to Cloudinary:", cloudinaryError);
+        return res.status(500).render("signup", { error: "Error uploading profile image" });
+      }
+    }
+
     await User.create({
       username,
       password,
       email,
-      profileImageUrl: req.file ? `/uploads/${req.file.filename}` : "/images/default.jpg",
+      profileImageUrl,
     });
-    
+
     return res.redirect("/user/login");
   } catch (error) {
-    console.log(error)
-    return res.render("signup", { error: error});
+    console.error("Error signing up user:", error);
+    return res.render("signup", { error: "Error signing up user" });
   }
 };
 
@@ -25,36 +41,56 @@ const loginUser = async (req, res) => {
     const token = await User.checkLoginAndGenerateToken(email, password);
     res.cookie("token", token).redirect("/");
   } catch (error) {
-    res.render("login",{error:"Email or password incorrect !!"})
+    console.error("Error logging in user:", error);
+    res.render("login", { error: "Email or password incorrect !!" });
   }
 };
 
-
 const logoutUser = async (req, res) => {
-  return res.clearCookie("token").redirect(`/`);
+  try {
+    return res.clearCookie("token").redirect(`/`);
+  } catch (error) {
+    console.error("Error logging out user:", error);
+    return res.status(500).send("Internal Server Error");
+  }
 };
 
 const displayUser = async (req, res) => {
-  const blogs = await Blog.find({ creator: req.params.id })
-    .sort({ createdAt: -1 })
-    .populate("creator");
-  const bloguser= await User.findById(req.params.id);
-  return res.render("userprofile", {user:req.user, bloguser, blogs });
-};
-
-const changeProfileImage= async(req,res)=>{
   try {
-    const updateduser = await User.findOneAndUpdate({_id:req.user._id},{profileImageUrl: `/uploads/${req.file.filename}`}, { new: true });
-
-    if (updateduser) {
-
-      return res.render(`edituser`,{user:updateduser});
-    } 
+    const blogs = await Blog.find({ creator: req.params.id })
+      .sort({ createdAt: -1 })
+      .populate("creator");
+    const bloguser = await User.findById(req.params.id);
+    return res.render("userprofile", { user: req.user, bloguser, blogs });
   } catch (error) {
-    console.error('Error updating blog:', error);
+    console.error("Error displaying user profile:", error);
     return res.status(500).send("Internal Server Error");
   }
-}
+};
+
+const changeProfileImage = async (req, res) => {
+  try {
+    const result = await cloudinary.uploader.upload(req.file.path, {
+      folder: "profiles",
+    });
+
+    const updatedUser = await User.findOneAndUpdate(
+      { _id: req.user._id },
+      { profileImageUrl: result.secure_url },
+      { new: true }
+    );
+
+    if (updatedUser) {
+      return res.render("edituser", { user: updatedUser });
+    } else {
+      return res.status(404).send("User not found");
+    }
+  } catch (error) {
+    console.error("Error updating profile image:", error);
+    return res.status(500).send("Internal Server Error");
+  }
+};
+
 const handleEdit = async (req, res) => {
   const { username, email } = req.body;
 
@@ -63,12 +99,12 @@ const handleEdit = async (req, res) => {
       const existingUser = await User.findOne({ email });
 
       if (existingUser) {
-        return res.render('edituser', { error: 'User already exists' });
+        return res.render("edituser", { error: "User already exists" });
       }
 
       const newdata = {
         username,
-        email
+        email,
       };
 
       const updatedUser = await User.findOneAndUpdate(
@@ -79,15 +115,17 @@ const handleEdit = async (req, res) => {
 
       if (updatedUser) {
         const newToken = await generateToken(updatedUser);
-        res.cookie('token', newToken);
+        res.cookie("token", newToken);
         const payloadUser = validateToken(newToken);
         req.user = payloadUser;
 
         return res.redirect(`/user/${req.user._id}`);
+      } else {
+        return res.status(404).send("User not found");
       }
     } else {
       const newdata = {
-        username
+        username,
       };
 
       const updatedUser = await User.findOneAndUpdate(
@@ -98,38 +136,62 @@ const handleEdit = async (req, res) => {
 
       if (updatedUser) {
         const newToken = await generateToken(updatedUser);
-        res.cookie('token', newToken);
+        res.cookie("token", newToken);
         const payloadUser = validateToken(newToken);
         req.user = payloadUser;
         return res.redirect(`/user/${req.user._id}`);
+      } else {
+        return res.status(404).send("User not found");
       }
     }
   } catch (error) {
-    console.error('Error updating user:', error);
-    return res.status(500).send('Internal Server Error');
+    console.error("Error updating user:", error);
+    return res.status(500).send("Internal Server Error");
   }
 };
-const changePassword=async(req,res)=>{
-  const {email,password,newpassword}= req.body;
-  if (email !== req.user.email) {
-    return res.render("changePassword", { user: req.user, error: "Incorrect Email Address" });
-  }
-  
-  const user = await User.findOne({email}).select("+password");
-  const emailPasswordCorrect = await bcrypt.compare(password, user.password);
-  if(emailPasswordCorrect)
-  {
-    const updatedUser = await User.findOneAndUpdate(
-      { _id: user._id },
-      { password: await bcrypt.hash(newpassword, 10) },
-      { new: true }
-    );
-    
-    return res.redirect(`/user/${req.user._id}`);
-  }
-  else{
-    return res.render("changePassword",{user,error:"Enter correct password"});
-  }
-}
 
-export { signupUser, loginUser, logoutUser, displayUser,changeProfileImage ,handleEdit,changePassword};
+const changePassword = async (req, res) => {
+  const { email, password, newpassword } = req.body;
+  try {
+    if (email !== req.user.email) {
+      return res.render("changePassword", {
+        user: req.user,
+        error: "Incorrect Email Address",
+      });
+    }
+
+    const user = await User.findOne({ email }).select("+password");
+    if (!user) {
+      return res.render("changePassword", { user: req.user, error: "User not found" });
+    }
+
+    const emailPasswordCorrect = await bcrypt.compare(password, user.password);
+    if (emailPasswordCorrect) {
+      await User.findOneAndUpdate(
+        { _id: user._id },
+        { password: await bcrypt.hash(newpassword, 10) },
+        { new: true }
+      );
+
+      return res.redirect(`/user/${req.user._id}`);
+    } else {
+      return res.render("changePassword", {
+        user: req.user,
+        error: "Enter correct password",
+      });
+    }
+  } catch (error) {
+    console.error("Error changing password:", error);
+    return res.status(500).send("Internal Server Error");
+  }
+};
+
+export {
+  signupUser,
+  loginUser,
+  logoutUser,
+  displayUser,
+  changeProfileImage,
+  handleEdit,
+  changePassword,
+};
